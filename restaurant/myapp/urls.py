5b01000e4f589ced7ab2f3ab9f4791557a1c0505
rabijati <jattirabi07@gmail.com>
2025-04-29 16:00:13 +0545

    Updated templates and added static files. Organized project structure with base template.

--- restaurant/myapp/urls.py
+++ restaurant/myapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path("",index,name="index"),
+    path("about/",about,name="about"),
+    path("services/",services,name="services"),
+    path("contact/",contact,name="contact"),
+    path("menu/",menu,name="menu"),
+
     
 ]
--- restaurant/myapp/views.py
+++ restaurant/myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request,'app/index.html')
+def about(request):
+    return render(request,'app/about.html')
+def contact(request):
+    return render(request,'app/contact.html')
+def menu(request):
+    return render(request,'app/menu.html')
+def services(request):
+    return render(request,'app/services.html')
+
+
